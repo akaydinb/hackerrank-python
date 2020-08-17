d17b040d83ae2c6815091a4ef6e1f29abbd32efe
akaydinb <9348636+akaydinb@users.noreply.github.com>
2020-08-17 16:49:36 +0200

    Initial code for "The Minion Game".

--- hackerrank.py
+++ hackerrank.py
@@ -1,63 +1,33 @@
 #!/usr/bin/python3
 
-# Piling Up!
-from collections import deque
+def minion_game(string):
+    stuart = set();
+    kevin  = set();
+    
+    for i in range(len(string)):
+        for j in range(i, len(string)):
+            substr = string[i:j+1];
+            if substr[0] in ['A', 'E', 'I', 'O', 'U']:
+                kevin.add(substr);
+            else:
+                stuart.add(substr);
+                
+    Spoints = 0;
+    Kpoints = 0;
+    
+    for x in stuart:
+        Spoints = Spoints + string.count(x);
+        
+    for x in kevin:
+        Kpoints = Kpoints + string.count(x);
+        
+    if Spoints > Kpoints:
+        print("Stuart", Spoints);
+    elif Kpoints > Spoints:
+        print("Kevin", Kpoints);
+    elif Spoints == Kpoints:
+        print("Draw");
 
 if __name__ == '__main__':
-    for T in range(int(input())):
-        n = int(input());
-        L = list(map(int, input().split()));
-        D = deque(L);
-        L.sort(reverse = True);
-
-        for k in range(n):
-            if L[k] == D[-1]:
-                D.pop();
-            elif L[k] == D[0]:          
-                D.popleft();
-            else:
-                print("No");
-                break;
-            
-            if len(D) <= 2:
-                print("Yes");
-                break;
-            
-#################################
-            ##ind, M = max(enumerate(L), key = operator.itemgetter(1));
-            #RM1 = L.pop();
-            #if ( RM1 < L[-1] ):
-                #print("No");
-                #break;
-            
-            #LM1 = L.popleft();
-            #if ( LM1 < L[0] ):
-                #print("No");
-                #break;
-            
-            #if ( len(L) == 2):
-                #print("Yes");
-                #break;
-            #else:
-                #n = len(L);
-            
-            
-#################################
-            #M = max(L);
-            ##print("maxelement = ", M);
-            ##input();
-            #if L[0] == M:
-                ##L.pop(0);
-                #L.popleft();
-                #n = n - 1;
-            #elif L[-1] == M:
-                ##L.pop(-1);
-                #L.pop();
-                #n = n - 1;
-            #else:
-                #print("No");
-                #break;
-
-            #if len(L) == 2:
-                #print("Yes");
-                #break;
+    s = input();
+    minion_game(s);
