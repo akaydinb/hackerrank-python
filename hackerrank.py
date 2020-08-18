7c166ad87330188496614190c6cc147a3136b7d5
akaydinb <9348636+akaydinb@users.noreply.github.com>
2020-08-18 16:40:31 +0200

    interim save

--- hackerrank.py
+++ hackerrank.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
+from itertools import combinations
+
 def minion_game(string):
     stuart = set();
     kevin  = set();
-    
-    for i in range(len(string)):
-        for j in range(i, len(string)):
-            substr = string[i:j+1];
-            if substr[0] in ['A', 'E', 'I', 'O', 'U']:
-                kevin.add(substr);
-            else:
-                stuart.add(substr);
-                
+#    for i in range(len(string)):
+#        for j in range(i, len(string)):
+#            substr = string[i:j+1];
+#            if substr[0] in ['A', 'E', 'I', 'O', 'U']:
+#                kevin.add(substr);
+#            else:
+#                stuart.add(substr);
+    substr = set( string[x:y] for x, y in combinations(range(len(string) + 1), r = 2) );
+    for astr in substr:
+        if astr[0] in ['A', 'E', 'I', 'O', 'U']:
+            kevin.add(astr);
+        else:
+            stuart.add(astr);
+
     Spoints = 0;
     Kpoints = 0;
     
@@ -21,7 +28,10 @@ def minion_game(string):
     for x in stuart:
         print("[", Spoints, "]", x, string.count(x));
         Spoints = Spoints + string.count(x);
-        
+
+#https://stackoverflow.com/questions/37499968/finding-all-repeated-substrings-in-a-string-and-how-often-they-appear
+#https://stackoverflow.com/questions/5616822/python-regex-find-all-overlapping-matches
+
     for x in kevin:
         print("[", Kpoints, "]", x, string.count(x));
         Kpoints = Kpoints + string.count(x);
